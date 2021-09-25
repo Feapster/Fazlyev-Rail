f40a0e5a2349b6abc3b97d1c4348a17f0daa0b2c
Фазлыев Раиль <feapster@gmail.com>
2021-09-25 08:46:07 +0300

    T

--- 4.py
+++ 4.py
@@ -1,21 +1,17 @@
 a = 'автор'
 b = 'товар'
 d1 = {}
-d2 = {}
 
 for i in range(len(a)):
     if a[i] in d1:
         d1[a[i]] += 1
     else:
         d1[a[i]] = 1
+for i in range(len(a)):
+    if b[i] in d1:
+        del d1[b[i]]
 
-for i in range(len(b)):
-    if b[i] in d2:
-        d2[b[i]] += 1
-    else:
-        d2[b[i]] = 1
-
-if d1 == d2:
+if d1 == {}:
     print('Да')
 else:
     print("Нет")
