d2743a0e49d4af417b4c57501b230804fd9d3df7
Фазлыев Раиль <feapster@gmail.com>
2021-10-16 14:12:22 +0300

    Е

--- 3/2.py
+++ 3/2.py
@@ -1,13 +1,17 @@
 from random import randint
 
-a = [randint(1,10) for i in range(12)]
+a = [randint(1,9) for i in range(7)]
 print(a)
-max_0 = 0
-for i in range(len(a)):
-    for j in range(len(a)):
-        min_1 = min(a[i], a[j])
-        max_1 = max(a[i], a[j])
-        s = (i - j) * (min_1 + (max_1 - min_1)*(1/2))
-        if max_0 < s:
-            max_0 = s
-print(max_0)
+
+i_1 = 0
+i_2 = len(a) - 1
+max = (i_2 - i_1) * (a[i_1] + a[i_2]) * (1/2)
+for i in range(len(a) - 1):
+    if a[i_1] < a[i_2]:
+        i_1 += 1
+    else:
+        i_2 -= 1
+    s = (i_1 - i_2) * (a[i_1] + a[i_2]) * (1/2)
+    if s > max:
+        max = s
+print(max)
