4d489864141048a0dbddebd42735eb0e767f2d4c
Фазлыев Раиль <feapster@gmail.com>
2021-12-04 10:29:20 +0300

    Е

--- Контрольная Работа/3.py
+++ Контрольная Работа/3.py
@@ -8,11 +8,11 @@ a = [[randint(0, nums) for i in range(n)] for j in range(n)]
 b = []
 
 for i in range(nums + 1):
-    b.append([i, 0])
+    b.append(i)
 
 for i in range(n):
     for j in range(n):
-        b[a[i][j]][1] += 1
+        b[a[i][j]] += 1
 
 for i in range(nums + 1):
-    print(i, " -", b[i][1], "times")
+    print(i, " -", b[i], "times")
